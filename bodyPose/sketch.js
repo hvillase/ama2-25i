f1d16ea1576fa05bad215f54f2b033c5510bbbe8
hvillase <hernani.vr@gmail.com>
2025-03-19 10:33:53 -0600

    todo

---
+++ bodyPose/sketch.js
@@ -0,0 +1,79 @@
+let bodyPose;
+let video;
+let poses = [];
+let connections;
+let painting;
+
+function preload() {
+  // Load the bodyPose model
+  bodyPose = ml5.bodyPose();
+}
+
+function mousePressed() {
+  console.log(poses);
+}
+
+function setup() {
+  createCanvas(640, 480);
+    // Create the video and hide it
+    video = createCapture(VIDEO);
+    video.size(640, 480);
+    video.hide();
+    // Start detecting poses in the webcam video
+    bodyPose.detectStart(video, gotPoses);
+    // Get the skeleton connection information
+    connections = bodyPose.getSkeleton();
+  }
+
+  // Callback function for when the model returns pose data
+function gotPoses(results) {
+  // Store the model's results in a global variable
+  poses = results;
+}
+
+function draw() {
+  // estos son los cuadros que queremos interactivos
+  noStroke();
+  fill(255, 0, 0, 0.5);
+  rect(width/2, 0, width/2, height/2);
+  fill(0, 0, 255, 0.5);
+  rect(0, 0, width/2, height/2);
+  //fill(0, 255, 255, 0.5);
+  //rect(0, 0, width, height);
+
+  // Mostrar el video. si lo comentamos se hace una retroalimentación
+  image(video, 0, 0, width, height);
+
+    // Dibujar  las conexiones del esqueleto, lo comentamos para no verlo
+    /*for (let i = 0; i < poses.length; i++) {
+      let pose = poses[i];
+      for (let j = 0; j < connections.length; j++) {
+        let pointAIndex = connections[j][0];
+        let pointBIndex = connections[j][1];
+        let pointA = pose.keypoints[pointAIndex];
+        let pointB = pose.keypoints[pointBIndex];
+              // Only draw a line if we have confidence in both points
+      if (pointA.confidence > 0.1 && pointB.confidence > 0.1) {
+        stroke(255, 0, 0);
+        strokeWeight(2);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+      }
+    }
+  }*/
+
+    // Iterar através de todas las poses
+    for (let i = 0; i < poses.length; i++) {
+      let pose = poses[i];
+          // Iterate through all the keypoints for each pose
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      let keypoint = pose.keypoints[j];
+            // Only draw a circle if the keypoint's confidence is greater than 0.1
+            if (keypoint.confidence > 0.1) {
+              fill(0, 255, 0);
+              noStroke();
+              circle(keypoint.x, keypoint.y, 10);
+            }
+          }
+        }
+      }
+
---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220,0,0);
+  circle(width/2, height/2, 75);
+  rect(50, 100, 60, 60);
+}
